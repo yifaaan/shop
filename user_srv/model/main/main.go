108817de0780481399e9d687f3b1335ff342cab9
Liuyifan <0219lyf@gmail.com>
2026-01-26 10:55:15 +0800

    init and create user table

---
+++ user_srv/model/main/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+	"shop/user_srv/model"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+func main() {
+	dsn := "shop_user:shop123456@tcp(127.0.0.1:3306)/shop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Disable color
+		},
+	)
+
+	// Globally mode
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 表明默认为单数
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	_ = db.AutoMigrate(&model.User{})
+}
