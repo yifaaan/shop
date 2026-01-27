c6def7bf6cc25d1d36ccb79bde7b5b0b0017de47
Liuyifan <0219lyf@gmail.com>
2026-01-27 11:02:04 +0800

    add jwt config

--- go.mod
+++ go.mod
@@ -53,6 +53,7 @@ require (
 
 require (
 	github.com/gin-gonic/gin v1.11.0
+	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	go.uber.org/zap v1.27.1
--- shop_api/user_web/config/config.go
+++ shop_api/user_web/config/config.go
@@ -1,8 +1,23 @@
 package config
 
+// UserSrvConfig 用户rpc服务配置
+type UserSrvConfig struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+// ServerConfig 服务配置
 type ServerConfig struct {
-	Name    string `mapstructure:"name"`
-	Version string `mapstructure:"version"`
-	IP      string `mapstructure:"ip"`
-	Port    int    `mapstructure:"port"`
+	Name       string        `mapstructure:"name"`
+	Version    string        `mapstructure:"version"`
+	IP         string        `mapstructure:"ip"`
+	Port       int           `mapstructure:"port"`
+	UserSrvCfg UserSrvConfig `mapstructure:"user_srv"`
+	JWTConfig  JWTConfig     `mapstructure:"jwt"`
+}
+
+//
+type JWTConfig struct {
+	SigningKey string `mapstructure:"key"`
+	ExpiresAt  int64  `mapstructure:"exp"`
 }
