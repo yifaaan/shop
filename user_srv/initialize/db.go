ad69540580a45b39449109b99b10fcccc773e623
Liuyifan <0219lyf@gmail.com>
2026-01-28 14:43:03 +0800

    export config file

--- user_srv/initialize/db.go
+++ user_srv/initialize/db.go
@@ -25,7 +25,6 @@ func InitDB() {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-
 			SlowThreshold: time.Second,   // Slow SQL threshold
 			LogLevel:      logger.Silent, // Log level
 			Colorful:      true,          // Disable color
