8ed2c3391f9dfc03400c8c79620cccf07745deec
Liuyifan <0219lyf@gmail.com>
2026-06-29 22:25:44 +0800

    Remove user-related gRPC proto definitions and associated generated files, along with user and base router implementations, utility functions for port retrieval, and mobile number validation logic. Introduce a new adaptor file in the backend

--- order_web/form/shop_cart.go
+++
@@ -1,6 +0,0 @@
-package form
-
-type ShopCartItemForm struct {
-	GoodId int32 `json:"good" form:"good" binding:"required"`
-	Nums   int32 `json:"nums" form:"nums" binding:"required,min=1"`
-}
--- user_web/form/sms.go
+++
@@ -1,6 +0,0 @@
-package form
-
-type SendSmsForm struct {
-	Mobile string `form:"mobile" json:"mobile" binding:"required,mobile"`
-	Type   int    `form:"type" json:"type" binding:"required,oneof=1 2 "` // 1表示注册，2表示登录
-}
